6f32ae81e17a5da16cab03612102175eb3527f7c
= <=>
2019-07-13 16:32:06 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+ document.addEventListener("DOMContentLoaded", function() {
+ 	console.log("Library của bs4 @ver4.3 đã kết nối");
+
+
+	//write code here...
+	alert("chào cậu");
+
+	// link javascript	
+
+
+});
